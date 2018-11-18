5854f25160a0d64a1ef1cb8014bc245c0c2be5d0
Sina Parvizi <snparvizi75@gmail.com>
2018-11-18 16:16:42 +0330

    submit project core

--- wp-content/themes/freelanceengine/post-project-core.php
+++ wp-content/themes/freelanceengine/post-project-core.php
@@ -1,7 +1,19 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sina
- * Date: 11/18/18
- * Time: 2:42 PM
- */
+
+                <div class ="page-post-project-wrap" id="post-place">
+                    <?php
+                    // check disable payment plan or not
+                    $disable_plan = ae_get_option('disable_plan', false);
+                    if(!$disable_plan) {
+                        // template/post-place-step1.php
+                        get_template_part( 'template/post-project', 'step1' );
+                    }
+
+                    // template/post-place-step3.php
+                    get_template_part( 'template/post-project', 'step3' );
+
+                    if(!$disable_plan) {
+                        // template/post-place-step4.php
+                        get_template_part( 'template/post-project', 'step4' );
+                    }
+                    ?>
+                </div>
