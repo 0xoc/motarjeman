653ac385881f68aa8c7739a2a54c8d0c1e5ecd99
Sina Parvizi <snparvizi75@gmail.com>
2018-11-18 16:13:39 +0330

    dashboard menu

---
+++ wp-content/themes/freelanceengine/dashboard-menu.php
@@ -0,0 +1,26 @@
+<style>
+    .dashboard-item{
+        margin-bottom: 10px;
+    }
+</style>
+
+<ul class="list-group dashboard-items">
+    <li class="list-group-item active dashboard-item">
+        ثبت پروژه
+    </li>
+    <li class="list-group-item active dashboard-item">
+        فهرست پروژه ها
+    </li>
+    <li class="list-group-item active dashboard-item">
+        ویرایش پروفایل
+    </li>
+    <li class="list-group-item active dashboard-item">
+        اطلاعیه ها
+    </li>
+    <li class="list-group-item active dashboard-item">
+        نمونه کار ها
+    </li>
+    <li class="list-group-item active dashboard-item">
+        اطلاعات حساب بانکی
+    </li>
+</ul>
